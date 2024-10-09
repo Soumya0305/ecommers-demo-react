2158003f37b5f0c53210171b7f55ed52625c1cd0
Soumya <user.soumya.jha.0501@gmail.com>
2024-10-10 02:21:45 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL= 'https://ecommerce-backend-z0q3.onrender.com'
+//const API_URL = 'http://localhost:5001/api';
 
 export const registerUser = async (userData) => {
   return await axios.post(`${API_URL}/auth/register`, userData);
