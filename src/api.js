ec8a90de11a8b88f8f2e346358b77a48b465ccb9
Soumya <user.soumya.jha.0501@gmail.com>
2024-10-10 10:48:57 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL= 'https://ecommerce-backend-z0q3.onrender.com'
+const API_URL= 'https://ecommerce-backend-z0q3.onrender.com/api'
 //const API_URL = 'http://localhost:5001/api';
 
 export const registerUser = async (userData) => {
